ad6696aaea8ccd331a508fdabbe1b0572f93af86
Cibele Watt <cibelewatt@gmail.com>
2021-08-30 21:49:14 -0300

    commit only?

---
+++ Herenca/Program.cs
@@ -0,0 +1,29 @@
+﻿using System; 
+
+namespace Heranca
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var contaOrigem = new ContaInvestimento(10);
+
+            var a = ConverteConta<ContaInvestimento, ContaCorrente>(contaOrigem);
+
+            Console.ReadLine();
+            
+        }
+
+        public static G ConverteConta<T, G>(T contaOrigem) where T : Conta where G : Conta
+        {
+            var b = Activator.CreateInstance<G>();
+            b.Numero = contaOrigem.Numero;
+            b.Saldo = contaOrigem.Saldo;
+            b.Agencia = contaOrigem.Agencia;
+
+            return b;
+            
+        }
+
+    }
+}
